2f441a1a85f45cb0ef15a5e36052345b2fcf5e85
Phil Mauro <philmauro13@users.noreply.github.com>
2026-04-09 23:05:09 -0600

    Deploy Supabase config

---
+++ config.js
@@ -0,0 +1,4 @@
+window.TOURHQ_SUPABASE = {
+  url: "https://edxiwaduxwaitxlahkzl.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeGl3YWR1eHdhaXR4bGFoa3psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MzU5NDAsImV4cCI6MjA5MTExMTk0MH0.8qsJpjcw0e56LOzB0bPSwlWe5AX4_nGc_tjmAKbTYtE"
+};
